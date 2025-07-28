81f87be4a211491330c1aa0c9948d551ff9b84fd
temmy669 <87032950+temmy669@users.noreply.github.com>
2025-07-28 16:44:43 +0100

    Update settings.py

--- imprest_portal/settings.py
+++ imprest_portal/settings.py
@@ -98,10 +98,10 @@ AZURE_AD_LOGOUT_URL = f"{AZURE_AD_AUTHORITY}/oauth2/v2.0/logout?post_logout_redi
 # if DEBUG:
 CORS_ALLOW_ALL_ORIGINS = True
 # else:
-#     CORS_ALLOWED_ORIGINS = [
-#         "https://172.16.0.95",
-#         "https://calloverapp.wajesmarthrms.website",
-#     ]
+CORS_ALLOWED_ORIGINS = [
+        "https://localhost:5173",
+         "https://calloverapp.wajesmarthrms.website",
+    ]
 
 TEMPLATES = [
     {
