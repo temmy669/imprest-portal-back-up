15bd4ce5aea1aa9db58755c05a2fb0a0ea28b691
temmy <favouradebose@gmail.com>
2025-07-29 16:46:15 +0100

    updated cors allowed origins

--- imprest_portal/settings.py
+++ imprest_portal/settings.py
@@ -66,9 +66,9 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
