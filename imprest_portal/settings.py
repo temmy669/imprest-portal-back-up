8032aacb4cbdd83b972f2f4083a8b7fd22c6ce15
temmy <favouradebose@gmail.com>
2025-07-29 16:34:44 +0100

    updated cors allowed origins

--- imprest_portal/settings.py
+++ imprest_portal/settings.py
@@ -104,6 +104,7 @@ CORS_ALLOW_CREDENTIALS = False
 CORS_ALLOWED_ORIGINS = [
 
          "http://localhost:5173",
+         "http://localhost:3000",
          "https://calloverapp.wajesmarthrms.website",
 
     ]
