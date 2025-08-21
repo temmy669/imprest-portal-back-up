9e96ca1567583e04953e7e6b6fe0bb3566f1cd0f
temmy <favouradebose@gmail.com>
2025-08-21 01:50:47 +0100

    implemented Budget Management Endpoints

--- stores/urls.py
+++ stores/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import StoreByRegionView, RegionListView, AssignStoresToUserView, StoreListView, ListAreaManagersByRegion
+from .views import StoreByRegionView, RegionListView, AssignStoresToUserView, StoreListView, ListAreaManagersByRegion, StoreBudgetView
 
 urlpatterns = [
     path('regions', RegionListView.as_view(), name='list-regions'),
     path('region/<int:region_id>/', StoreByRegionView.as_view(), name= 'stores-by-region'),
     path('assign-stores/<int:user_id>/', AssignStoresToUserView.as_view(), name='assign-store'),
-    path('manager-region/', ListAreaManagersByRegion.as_view(), name="manager-region"),
+    path('manager-region/', ListAreaManagersByRegion.as_view(), name='manager-region'),
+    path('store-budgets/', StoreBudgetView.as_view(), name='store-budget'),
+    path('update-budget/<int:pk>/', StoreBudgetView.as_view(), name='update-budget'),
     path('', StoreListView.as_view(), name='store-list'),
 ]
