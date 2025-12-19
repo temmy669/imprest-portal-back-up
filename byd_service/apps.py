b7914fc2743c6060d65d17e702c915c9414f7e32
temmy <favouradebose@gmail.com>
2025-12-19 14:16:49 +0100

    Added ByD services.

---
+++ byd_service/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BydServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'byd_service'
+    verbose_name = '6. SAP ByD Service'
