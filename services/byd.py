2e03f0621412045c1b6c36a36f11c395c5dbf8c6
maruche <nwokohcha.maruche@gmail.com>
2026-02-12 08:55:08 +0100

    --x

--- services/byd.py
+++ services/byd.py
@@ -1,6 +1,9 @@
 import requests
 
 class BYD:
+    def __init__(self, url):
+        self.url = url
+        
     def get_expense_items(self):
         """Get the list of expense items from BYD. """
         pass
