d70a58e179afbbdd33eed2581801c1b21c3b8368
maruche <nwokohcha.maruche@gmail.com>
2026-03-03 16:06:34 +0100

    --x

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('auth/logout/', AzureLogoutView.as_view(), name='azure-logout'),
     path('users/', UserView.as_view(), name = 'user-list'),
     path('users/<int:pk>/', UserView.as_view(), name = 'user-detail'),
-    path('users/<int:pk>/edit', UpdateUserView.as_view()),
+    path('users/<int:pk>/edit/', UpdateUserView.as_view()),
     path("n0t-0k@y/", DummyLogin.as_view(), name="dummy-login"),
     path('auth/me/', MeView.as_view(), name='me'),
     path('user/search/', SearchUserView.as_view(), name= 'search-user'),
