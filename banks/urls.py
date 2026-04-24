9e4c4d34dc8b17efc845b51259de1daf277022e8
temmy <favouradebose@gmail.com>
2026-04-24 17:03:29 +0100

    fix: correct url pattern for bank detail view

--- banks/urls.py
+++ banks/urls.py
@@ -3,8 +3,8 @@ from .views import BankView, AccountView, AccountListByBankView, BankListView, L
 
 urlpatterns = [
     path('', BankView.as_view(), name='bank-list'),
-    path('<str:pk>/', BankView.as_view(), name='bank-detail'),
     path('list-banks/', BankListView.as_view(), name='bank-names-list'),
+    path('<str:pk>/', BankView.as_view(), name='bank-detail'),
     path('byd/', ListBanksView.as_view(), name='list-banks-byd'),
     path('accounts/', AccountView.as_view(), name='account-list'),
     path('<uuid:bank_id>/accounts/', AccountListByBankView.as_view(), name='account-list-by-bank'),
